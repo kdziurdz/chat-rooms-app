5f1e30e638ec460328b8453d598bca9118cbe378
Konrad Dziurdź <kodz@grapeup.com>
2019-03-20 09:32:19 +0100

    cors all

--- src/main/java/com/grape/chat/chatapp/config/WebConfiguration.java
+++ src/main/java/com/grape/chat/chatapp/config/WebConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://cf-demo-react-busy-sable.cfapps.io/");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
